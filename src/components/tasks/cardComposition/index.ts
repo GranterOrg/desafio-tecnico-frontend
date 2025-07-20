77a9192c447891977388d2843766b99bd486707a
diegoporfirio01 <porfiriodiego64@gmail.com>
2025-07-20 12:37:35 -0300

    feat: add index TaskCard component ! and TasksView !

---
+++ src/components/tasks/cardComposition/index.ts
@@ -0,0 +1,6 @@
+export { default as TaskCardRoot } from "./TaskCardRoot.vue";
+export { default as TaskCardID } from "./TaskCardID.vue";
+export { default as TaskCardContent } from "./TaskCardContent.vue";
+export { default as TaskCardInfo } from "./TaskCardInfo.vue";
+export { default as TaskCardActions } from "./TaskCardActions.vue";
+export { default as TaskCardComposed } from "./TaskCardComposed.vue";
---
+++ src/views/TasksView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Header from "@/components/layout/Header.vue";
+import Breadcrumbs from "@/components/layout/Breadcrumbs.vue";
+import TabPanel from "@/components/layout/tabs/TabPanel.vue";
+import Tabs from "@/components/layout/tabs/Tabs.vue";
+import Container from "@/components/layout/Container.vue";
+</script>
+
+<template>
+  <Header>
+    <Breadcrumbs :include-query-params="true" />
+  </Header>
+  <Container>
+    <Tabs>
+      <TabPanel />
+    </Tabs>
+  </Container>
+</template>
